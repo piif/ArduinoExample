18b8e5d457b72dc9c59384886f55d79058665fc8
piif <christian_lefebvre@laposte.net>
2014-10-22 22:09:21 +0200

    use defines to make code able to compile from my tool chain AND Arduino IDE

--- ArduinoExample.ino
+++ ArduinoExample.ino
@@ -1,4 +1,16 @@
-#include "led7/led7.h"
+#ifdef PIF_TOOL_CHAIN
+	#include <Arduino.h>
+	// other includes with full pathes
+	// example : #include "led7/led7.h"
+#else
+	// other includes with short pathes
+	// example : #include "led7.h"
+#endif
+
+#ifndef DEFAULT_BAUDRATE
+	#define DEFAULT_BAUDRATE 115200
+#endif
+
 
 void setup(void) {
 	pinMode(13, OUTPUT);
